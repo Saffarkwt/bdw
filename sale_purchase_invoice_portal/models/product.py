615190b9c583bd36cbf74e76c05f42d42fe711e1
Saffarkwt <saffarkwt@gmail.com>
2022-12-15 14:33:16 +0530

    [ADD] add warrenty and cost code on product label.

--- sale_purchase_invoice_portal/models/product.py
+++ sale_purchase_invoice_portal/models/product.py
@@ -4,9 +4,12 @@ from odoo import models, api, fields
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    def get_attributes(self):
+    def get_warranty(self):
         for line in self.attribute_line_ids:
-            str = ''
-            if line.attribute_id.name in ['Warranty date', 'Cost']:
-                str += line.value_ids[0].name + ','
-            return str
+            if line.attribute_id.name == 'Warranty date':
+                return line.value_ids[0].name
+
+    def get_cost_code(self):
+        for line in self.attribute_line_ids:
+            if line.attribute_id.name == 'Cost':
+                return line.value_ids[0].name
