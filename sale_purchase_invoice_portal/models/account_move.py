0d891f0acb449d0bb97fda21b470d18f512fa114
Saffarkwt <saffarkwt@gmail.com>
2022-12-21 15:29:05 +0530

    [MOD] add changes as per requirement.

--- sale_purchase_invoice_portal/models/account_move.py
+++ sale_purchase_invoice_portal/models/account_move.py
@@ -4,6 +4,8 @@ from odoo import models, api, fields
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+    civil_id = fields.Char(related="partner_id.email")
+
     def is_consignment(self):
         po = self.purchase_id.search([('name','=',self.invoice_origin)])
         return True if po.is_consignment else False
