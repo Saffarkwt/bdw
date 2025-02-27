47293ea9d04edc884815d262e3a7457ff2fa76fe
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-02-27 09:30:20 +0300

    Add files via upload

--- sale_purchase_invoice_portal/__manifest__.py
+++ sale_purchase_invoice_portal/__manifest__.py
@@ -1,17 +1,19 @@
 {
     "name": "Sale Purchase Invoice portal",
-    "version": "15.0.0.0",
+    "version": "18.0.0.0",
     "category": "",
     "summary": "",
     "description": """ """,
-    "depends": ["account",'sale'],
+    "depends": ["account",'sale', 'purchase'],
+    "license": 'LGPL-3',
     "data": [
-        "report/invoice_report.xml",
+        #"report/invoice_report.xml",
         "report/invoice_report_templates.xml",
         "report/invoice_report_refund.xml",
         "report/product_label_report.xml",
         "views/account_move_view.xml",
-        "views/purchase_views.xml"
+        "views/purchase_views.xml",
+        "views/partner_views.xml"
     ],
     "demo": [],
     "installable": True,
