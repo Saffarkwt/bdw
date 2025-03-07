4c6dbd65a9bf831d999fc253233d0f0dad75836c
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-03-07 03:09:01 +0300

    Add files via upload

--- sale_purchase_invoice_portal/models/purchase_order.py
+++ sale_purchase_invoice_portal/models/purchase_order.py
@@ -1,4 +1,4 @@
-from odoo.tests import Form
+#from odoo.tests import Form
 from odoo.fields import Date
 from odoo import models, api, fields
 
@@ -11,8 +11,6 @@ class PurchaseOrder(models.Model):
     def button_confirm(self):
         res = super(PurchaseOrder, self).button_confirm()
         action = self.picking_ids.button_validate()
-        wizard = Form(self.env[action['res_model']].with_context(action['context'])).save()
-        wizard.process()
         if not self.is_consignment:
             action = self.action_create_invoice()
             invoice = self.env['account.move'].browse(action['res_id'])
