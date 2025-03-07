01e972b81e78e5ddce968bed95d413774323c993
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-03-07 03:14:52 +0300

    Add files via upload

--- sr_customization/models/product.py
+++ sr_customization/models/product.py
@@ -5,25 +5,15 @@ class Product(models.Model):
     _inherit = "product.product"
     _order = "create_date desc"
 
-    @api.model
+    @api.model_create_multi
     def create(self,vals):
         if self.env.context.get('default_type_product'):
-            vals.update({'type':'product'})
+            vals.update({'type':'consu', 'is_storable': True})
         res = super(Product,self).create(vals)
         return res
 
     @api.model
     def default_get(self, default_fields):
         values = super(Product, self).default_get(default_fields)
-        values.update({'type': 'product'})
-        return values
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-    _order = "create_date desc"
-
-    @api.model
-    def default_get(self, default_fields):
-        values = super(ProductTemplate, self).default_get(default_fields)
-        values.update({'detailed_type': 'product'})
+        values.update({'type': 'consu', 'is_storable': True})
         return values
