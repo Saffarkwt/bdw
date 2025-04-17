682b8988cc1c597f4231b41a93b339ec55b0ed46
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-04-17 20:46:13 +0300

    Add files via upload

--- stock_display_receipt/__manifest__.py
+++ stock_display_receipt/__manifest__.py
@@ -6,9 +6,9 @@
     "author": "Your Company",
     "depends": ["stock"],
     "data": [
-        "views/stock_picking_display_receipt_button.xml",
         "report/stock_display_receipt_template.xml",
-        "report/stock_display_receipt_report.xml"
+        "report/stock_display_receipt_report.xml",
+        "views/stock_picking_display_receipt_button.xml"
     ],
     "installable": True,
     "application": False,
