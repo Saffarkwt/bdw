0e1562583d351a71de96288691dbfc84703d67f4
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-04-17 21:55:21 +0300

    Add files via upload

--- stock_display_receipt/models/stock_picking.py
+++ stock_display_receipt/models/stock_picking.py
@@ -4,10 +4,4 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def print_display_receipt(self):
-        return {
-            'type': 'ir.actions.report',
-            'report_name': 'stock_display_receipt.report_display_receipt',
-            'report_type': 'qweb-pdf',
-            'model': 'stock.picking',
-            'res_ids': self.ids,
-        }
+        return self.env.ref('stock_display_receipt.action_display_receipt_report').report_action(self)
