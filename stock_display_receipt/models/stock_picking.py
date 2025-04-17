fb222e9baacdcf5136fe961ce7c1b478a3a0dc95
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-04-17 21:25:44 +0300

    Add files via upload

--- stock_display_receipt/models/stock_picking.py
+++ stock_display_receipt/models/stock_picking.py
@@ -4,4 +4,4 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def print_display_receipt(self):
-        return self.env.ref('stock_display_receipt.action_display_receipt_report').report_action(self)
+        return self.env.ref('stock_display_receipt.action_display_receipt_report').report_action(self.ids)
