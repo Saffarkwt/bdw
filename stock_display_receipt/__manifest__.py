af4cd98cd9530f0b1dcc28bc3ff2408643baaaa4
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-04-17 17:38:09 +0300

    Add files via upload

---
+++ stock_display_receipt/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Stock Display Receipt",
+    "version": "1.0",
+    "category": "Inventory",
+    "summary": "Custom Display Receipt for Stock Picking",
+    "author": "Your Company",
+    "depends": ["stock"],
+    "data": [
+        "report/stock_display_receipt_template.xml",
+        "report/stock_display_receipt_report.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
