cda60990958be830ba65d31d464e224302f37e68
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-04-17 21:35:23 +0300

    Add files via upload

--- stock_display_receipt/models/stock_picking.py
+++
@@ -1,7 +0,0 @@
-from odoo import models
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    def print_display_receipt(self):
-        return self.env.ref('stock_display_receipt.action_display_receipt_report').report_action(self.ids)
