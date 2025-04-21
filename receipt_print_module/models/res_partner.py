9d02e4f151c37b77676aa4b90b4b67c7284195e8
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-04-21 07:21:55 +0300

    Delete receipt_print_module directory

--- receipt_print_module/models/res_partner.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-    
-    civil_id = fields.Char(string='Civil ID', help='Civil ID number of the partner')
