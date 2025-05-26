e1476b3abcf9bf469e2c411d2a9e022983aaa6f9
Saffarkwt <111451383+Saffarkwt@users.noreply.github.com>
2025-05-26 20:04:18 +0300

    Add files via upload

--- sr_customization/__manifest__.py
+++ sr_customization/__manifest__.py
@@ -2,14 +2,26 @@
     "name": "Sr Customization",
     "version": "18.0.0.0",
     "category": "",
-    "summary": "",
-    "description": """ """,
-    "depends": ["account",'stock', 'purchase'],
+    "summary": "Custom Barcode Label Template",
+    "description": """
+    Custom module for Odoo 18 that provides:
+    - Modified barcode label template
+    - Custom product methods
+    """,
+    "author": "Mohamed Abdelrahman",
+    "email": "Mohammedeelsayd@gmail.com",
+    "website": "https://freelancer-kw.com",
+    "mobile": "+96598591476",
+    "depends": ["account",'stock', 'purchase', 'product'],
     "license": 'LGPL-3',
     "data": [
         "data/sequence.xml",
         "views/account_move.xml",
-        "views/purchase_view.xml"
+        "views/purchase_view.xml",
+        "report/product_label_report.xml"
+    ],
+    "images": [
+        "static/description/icon.png"
     ],
     "demo": [],
     "installable": True,
